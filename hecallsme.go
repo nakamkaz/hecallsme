909fba8749328e88d42ca957ba10d6cced8480b5
nakamkaz <nakamkaz@kazuo.dev>
2023-04-02 00:22:24 +0900

    date time and file path for Windows/Linux/Mac

--- hecallsme.go
+++ hecallsme.go
@@ -1,23 +1,32 @@
 package main
+
 import (
-"fmt"
-"os"
-"io/ioutil"
-"strconv"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"runtime"
+	"strconv"
+	"time"
 )
 
-const CRLF = "\r\n"
+func main() {
 
-func main(){
-content := ""
-
-for _,val := range os.Environ(){
-content += val + CRLF
-}
-content += "======" + CRLF
-for n, val := range os.Args {
-content += "Arg"+ strconv.Itoa(n)+ ": "+ val + CRLF
-}
-fmt.Println(content)
-ioutil.WriteFile("C:/temp/howcallsme.txt",[]byte(content),os.ModePerm)
+	dtstr := time.Now().Format("2006-01-02 15:04:05 Z0700")
+	content := ""
+	filedest := "C:/temp/hecallsme.txt"
+	CRLF := "\r\n"
+	if runtime.GOOS == "windows" {
+	} else {
+		filedest = "/tmp/hecallsme.txt"
+		CRLF = "\n"
+	}
+	for _, val := range os.Environ() {
+		content += val + CRLF
+	}
+	content += "==== " + dtstr + " ====" + CRLF
+	for n, val := range os.Args {
+		content += "Arg" + strconv.Itoa(n) + ": " + val + CRLF
+	}
+	fmt.Println(content)
+	ioutil.WriteFile(filedest, []byte(content), os.ModePerm)
 }
